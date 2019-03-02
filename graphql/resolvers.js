717d4ba82a95ed8ce69274230f4db17c157287a7
Taewoong Moon <moondingie5@gmail.com>
2019-03-02 17:24:25 +0900

    schema 추가

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,25 @@
+const nicolas = {
+    name: 'NiCol',
+    age: 18,
+    gender: 'female',
+}
+
 const resolvers = {
     Query: {
-        name: () => "Taewoong"
+        person: () => nicolas
     }
 }
 
 export default resolvers;
+
+// const nicolas = {
+//     name: 'taewoong',
+//     age: 20,
+//     gender: '123',
+// }
+
+// const resolvers = {
+//     Query: {
+//         person: () => nicolas
+//     }
+// }
